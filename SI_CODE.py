77e3bdcaf63c3c3d7b637bff97207bf65c3bb403
EnriqueRelav <luisvalrc@gmail.com>
2024-06-10 19:26:52 -0500

    Comente lo ultimo

--- SI_CODE.py
+++ SI_CODE.py
@@ -143,18 +143,17 @@ plt.show()
 
 #segun la grafica el numero optimo  K =2
 
-"""AQui ejecutaremos el kmeans y veremos los cluster """ 
+"""AQui ejecutaremos el kmeans y veremos los cluster 
 #aplicando el metodo kmeans a la bd 
 clustering= KMeans(n_clusters=2 , max_iter=3000) #crea el modelo 
 clustering.fit(df_final) #aplicamos el modelo a la bd 
 
-KMeans(algorithm='auto',copy_x=True,init ='k-means++', max_iter=300,n_clusters=2,n_init=10,n_jobs=None,precompute_distances='auto',random_state=None,tl=0.0001,verbose=0)
-
-
-"""Agregando la clasificacion al archivo original """
+KMeans(algorithm='auto',copy_x=True,init ='k-means++', max_iter=300,n_clusters=2,n_init='auto',n_job=None,precompute_distances='auto',random_state=None,tol=0.0001,verbose=0)
+""" 
+"""Agregando la clasificacion al archivo original  
 df_final['KMeans_clusters']=clustering.labels_
 df_final.head()
-
+"""
 """Visualizando los clusters que se formaron"""
 
 
